b63781b564a60e30580736424fd3bb24c30d5305
Lherner Remon <lherner21@gmail.com>
2022-06-15 17:08:11 -0500

    feat: add variables for module s3

--- variables.tf
+++ variables.tf
@@ -4,7 +4,47 @@ variable "project_name" {
 variable "project_environment" {
 }
 
-variable "site_domain" {
-  type = string
-  default = "*"
+variable "cors_allowed_headers" {
+  type = list(string)
+  default = ["*"]
+}
+
+variable "cors_allowed_methods" {
+  type = list(string)
+  default = ["GET", "HEAD", "POST"]
+}
+
+variable "cors_expose_headers" {
+  type = list(string)
+  default = []
+}
+
+variable "cors_allowed_origins" {
+  type = list(string)
+  default = ["*"]
+}
+
+variable "cors_max_age_seconds" {
+  type = number
+  default = 3000
+}
+
+variable "policy_block_public_acls" {
+  type = bool
+  default = false
+}
+
+variable "policy_ignore_public_acls" {
+  type = bool
+  default = false
+}
+
+variable "policy_block_public_policy" {
+  type = bool
+  default = false
+}
+
+variable "policy_restrict_public_buckets" {
+  type = bool
+  default = false
 }
